0bef4a1ad63983ec6652d6a806693b9b529e376f
Dark2Wings <96883668+Dark2Wings@users.noreply.github.com>
2023-02-05 17:51:02 +0300

    Update Director.java

--- src/olga/fedianina/people/Director.java
+++ src/olga/fedianina/people/Director.java
@@ -6,7 +6,8 @@ import olga.fedianina.school.School;
 
 
 public class Director extends People {
-
+    //здесь я бы задумался об отношении школы и директора. Не бывает директора без школы, так что возможно стоит задать ему такое поле, 
+    //но может быть это излишне, тут не уверен.
     public Director(String name, String surname, int age) {
         super(name, surname);
         if (age > 45) {
@@ -15,13 +16,15 @@ public class Director extends People {
             throw new IllegalArgumentException("Возраст должен быть не менее 45 лет");
         }
     }
-
+    
+    //Пустой конструктор вводит в небольшое заблуждение. Лучше написать фабричный метод, который вернет экземпляр класса
     public Director() {
         super();
         age = (int) (45 + Math.random() * 30);
     }
 
-
+    //во всех остальных случаях ты используешь другую форму глаголов, для единообразия лучше поставить hire.
+    //так же встает вопрос: а куда он их нанимает? Это же не его рабы. Я бы отпавил этот метод в класс школы, тогда и return, скорее всего, не понадобится
     public Teacher[] hiringTeachers() {
         Teacher[] teachers = new Teacher[BaseConst.MAX_TEACHER_COUNT];
         for (int i = 0; i < BaseConst.MAX_TEACHER_COUNT; ++i) {
