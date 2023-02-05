d3e5531d991627877aa77c5fb929a492a40e5f70
Dark2Wings <96883668+Dark2Wings@users.noreply.github.com>
2023-02-05 17:41:18 +0300

    Update Title.java

--- src/olga/fedianina/people/base/Title.java
+++ src/olga/fedianina/people/base/Title.java
@@ -1,14 +1,14 @@
 package olga.fedianina.people.base;
-
+//Title это название для статьи/текста/.., но не для людей. Лучше что-то в духе Names
 public enum Title {
     MAN_NAME(new String[]{"Михаил", "Владимир", "Никита", "Николай", "Сергей", "Инокентий", "Арсений"}),
     MAN_SURNAME(new String[]{"Иванов", "Петров", "Сидоров", "Васнецов", "Орлов", "Кузнецов"}),
     WOMAN_SURNAME(new String[]{"Иванова", "Петрова", "Сидорова", "Васнецова", "Орлова", "Кузнецова"}),
     WOMAN_NAME(new String[]{"Анна", "Олеся", "Юлия", "Светлана", "Инна", "Марина"});
 
-
+    //meaning - смысл, я бы взял простое values
     private String[] meaning;
-
+    //модификатор
     Title(String[] meaning) {
         this.meaning = meaning;
     }
